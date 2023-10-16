80d8218e9f4d7e2a7591796a6e86ad122ffe553e
Tina <thywang1@gmail.com>
2023-10-16 17:10:39 -0400

    feat: create deleteServiceRequestByID() function and interface

--- backend/typescript/services/implementations/serviceRequest.ts
+++ backend/typescript/services/implementations/serviceRequest.ts
@@ -1,13 +1,19 @@
+import { Prisma } from "@prisma/client";
 import IServiceRequest from "../interfaces/serviceRequest";
 import { getErrorMessage } from "../../utilities/errorUtils";
 import logger from "../../utilities/logger";
-import { Prisma } from "@prisma/client";
 import prisma from "../../prisma";
 
 const Logger = logger(__filename);
 
 class ServiceRequest implements IServiceRequest {
+  /* eslint-disable class-methods-use-this */
 
+  // get method here
+
+  // post method here
+
+  async deleteServiceRequestByID(requestId: string): Promise<void> {}
 }
 
 export default ServiceRequest;
--- backend/typescript/services/interfaces/serviceRequest.ts
+++ backend/typescript/services/interfaces/serviceRequest.ts
@@ -1,4 +1,14 @@
 interface IServiceRequest {
+  // get method here
+
+  // post method here
+
+  /**
+   * Delete a serviceRequest by id
+   * @param requestId serviceRequest's id
+   * @throws Error if serviceRequest deletion fails
+   */
+  deleteServiceRequestByID(requestId: string): Promise<void>;
 }
 
 export default IServiceRequest;
