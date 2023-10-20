1112594dbde46ad90c2c76e021e6b45ce6505b10
Emma Huang <emmahuang.ty@gmail.com>
2023-10-19 20:14:32 -0400

    created postServiceRequest function interface

--- backend/typescript/services/interfaces/serviceRequest.ts
+++ backend/typescript/services/interfaces/serviceRequest.ts
@@ -1,7 +1,16 @@
+import { CreateServiceRequestDTO, ServiceRequestDTO } from "../../types";
+
 interface IServiceRequest {
   // get method here
 
   // post method here
+  /**
+   * Create a serviceRequest
+   * @param serviceRequest the serviceRequest to be created
+   * @returns a ServiceRequestDTO with the created serviceRequest's information
+   * @throws Error if serviceRequest creation fails
+   */
+  postServiceRequest(serviceRequest: CreateServiceRequestDTO): Promise<ServiceRequestDTO>;
 
   /**
    * Delete a serviceRequest by id
