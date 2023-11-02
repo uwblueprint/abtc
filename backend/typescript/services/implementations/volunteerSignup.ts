6cb5289a8bfe5a14ec9bab2f68c794a62a97e373
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2023-11-02 19:33:19 -0400

    added bare bones volunteer signup implementation

---
+++ backend/typescript/services/implementations/volunteerSignup.ts
@@ -0,0 +1,19 @@
+import IVolunteerSignup from "../interfaces/volunteerSignup";
+
+class VolunteerSignup implements IVolunteerSignup{
+
+    async getVolunteerSignup(): Promise<void> {
+        
+    }
+
+    async postVolunteerSignup(): Promise<void> {
+        
+    }
+
+    async editVolunteerSignup(): Promise<void> {
+        
+    }
+
+}
+
+export default VolunteerSignup;
