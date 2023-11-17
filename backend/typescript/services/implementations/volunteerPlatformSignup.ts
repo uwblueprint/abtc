426a42101a0ea5df3a563d049535e3811b1dca39
annxiesun <anniesun@v1040-wn-rt-a-120-112.campus-dynamic.uwaterloo.ca>
2023-11-16 20:50:11 -0500

    add getVolunteerPlatformSignup logic

--- backend/typescript/services/implementations/volunteerPlatformSignup.ts
+++ backend/typescript/services/implementations/volunteerPlatformSignup.ts
@@ -1,19 +1,41 @@
+import { Prisma, volunteerPlatformSignUp } from "@prisma/client";
+import prisma from "../../prisma";
 import IVolunteerPlatformSignup from "../interfaces/volunteerPlatformSignup";
+import logger from "../../utilities/logger";
+import { getErrorMessage } from "../../utilities/errorUtils";
 
-class VolunteerPlatformSignup implements IVolunteerPlatformSignup{
-    // ADD PARAMETER AND RETURN TYPES IN NEXT TICKET
+const Logger = logger(__filename);
 
-    async getVolunteerPlatformSignup(): Promise<void> {
-        // Implementation to be added
+class VolunteerPlatformSignup implements IVolunteerPlatformSignup {
+  async getVolunteerPlatformSignup(
+    adminId: string,
+  ): Promise<volunteerPlatformSignUp[]> {
+    let volunteerSignUps: volunteerPlatformSignUp[];
+
+    try {
+      volunteerSignUps = await prisma.volunteerPlatformSignUp.findMany({
+        where: {
+          adminId,
+        },
+      });
+    } catch (error) {
+      Logger.error(
+        `Failed to get volunteer platform signups. Reason = ${getErrorMessage(
+          error,
+        )}`,
+      );
+      throw error;
     }
+    return volunteerSignUps;
+  }
 
-    async postVolunteerPlatformSignup(): Promise<void> {
-        // Implementation to be added
-    }
+  async postVolunteerPlatformSignup(): Promise<void> {
+    // Implementation to be added
+  }
 
-    async editVolunteerPlatformSignup(): Promise<void> {
-        // Implementation to be added
-    }
+  async editVolunteerPlatformSignup(): Promise<void> {
+    // Implementation to be added
+  }
 
 }
 
