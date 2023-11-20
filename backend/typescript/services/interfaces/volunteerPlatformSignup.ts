7076fef9347f736384f7a85aea5e83bce9850a4b
Yaser <yayonoss@gmail.com>
2023-11-19 21:08:16 -0500

    Added function signature for postVolunteerPlatformSignup

--- backend/typescript/services/interfaces/volunteerPlatformSignup.ts
+++ backend/typescript/services/interfaces/volunteerPlatformSignup.ts
@@ -1,3 +1,5 @@
+import { Prisma } from "@prisma/client";
+
 interface IVolunteerPlatformSignup {
     // ADD PARAMETER AND RETURN TYPES IN NEXT TICKET
 
@@ -8,8 +10,13 @@ interface IVolunteerPlatformSignup {
 
    /**
    * Posts volunteer signup
+   * @param volunteerPlatform the volunteerPlatform to be created
+   * @returns a volunteerPlatformSignUp with the provided information
+   * @throws Error if user creation fails
    */    
-    postVolunteerPlatformSignup(): Promise<void>
+    postVolunteerPlatformSignup(
+        volunteerPlatform: Prisma.volunteerPlatformSignUp,
+    ): Promise<Prisma.volunteerPlatformSignUp>
 
    /**
    * Edits volunteer signup
