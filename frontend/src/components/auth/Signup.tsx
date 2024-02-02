11e5724999aebc9a7cd7627a69c7977198ebf683
Tina <thywang1@gmail.com>
2024-02-01 20:20:32 -0500

    feat: create basic page layout

--- frontend/src/components/auth/Signup.tsx
+++ frontend/src/components/auth/Signup.tsx
@@ -1,79 +1,36 @@
-import React, { useContext, useState } from "react";
-import { Redirect } from "react-router-dom";
-
-import authAPIClient from "../../APIClients/AuthAPIClient";
-import { HOME_PAGE } from "../../constants/Routes";
-import AuthContext from "../../contexts/AuthContext";
-import { AuthenticatedUser } from "../../types/AuthTypes";
+import React from 'react';
+import { Flex, Box, Center, Heading, FormControl, FormHelperText, FormLabel, Input, Button, Text, Link } from '@chakra-ui/react'
 
 const Signup = (): React.ReactElement => {
-  const { authenticatedUser, setAuthenticatedUser } = useContext(AuthContext);
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onSignupClick = async () => {
-    const user: AuthenticatedUser = await authAPIClient.register(
-      firstName,
-      lastName,
-      email,
-      password,
-    );
-    setAuthenticatedUser(user);
-  };
-
-  if (authenticatedUser) {
-    return <Redirect to={HOME_PAGE} />;
-  }
-
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Signup</h1>
-      <form>
-        <div>
-          <input
-            type="text"
-            value={firstName}
-            onChange={(event) => setFirstName(event.target.value)}
-            placeholder="first name"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            value={lastName}
-            onChange={(event) => setLastName(event.target.value)}
-            placeholder="last name"
-          />
-        </div>
-        <div>
-          <input
-            type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            placeholder="username@domain.com"
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            placeholder="password"
-          />
-        </div>
-        <div>
-          <button
-            className="btn btn-primary"
-            type="button"
-            onClick={onSignupClick}
-          >
-            Sign Up
-          </button>
-        </div>
-      </form>
-    </div>
+    <Flex width="100vw" height="100vh" align="center" justify="center" background="dimgray">
+        <Box width="md" pt={16} pr={16} pl={16} pb={10} m={5} background="white" rounded={20}>
+          <Box pb={4} textAlign="left">
+            <Heading size='lg'>Create an Account</Heading>
+          </Box>
+          <Box my={4} textAlign="left">
+            <form>
+              <FormControl border={2} borderColor="#0B0B0B" isRequired>
+                <FormLabel>First Name</FormLabel>
+                <Input/>
+              </FormControl>
+              <FormControl mt={6} border={2} isRequired>
+                <FormLabel>Last Name</FormLabel>
+                <Input/>
+              </FormControl>
+              <Center mt={4} mb={4}>
+                <Button isDisabled width="full" mt={4} color="gray" background="#28214C" opacity="50%" type="submit">
+                  Next
+                </Button>
+              </Center>
+              <Center>
+                {/* todo: change href link to Login Page */}
+                <Text fontSize='sm'>Already have an account? <Link textDecoration="underline" href='https://chakra-ui.com' isExternal>Login</Link></Text>
+              </Center>
+            </form>
+          </Box>
+        </Box>
+    </Flex>
   );
 };
 
