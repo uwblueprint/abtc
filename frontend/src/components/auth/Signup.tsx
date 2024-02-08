8aac97f3c36f9c67d784120babb2bbe17e159103
Tina <thywang1@gmail.com>
2024-02-07 19:52:28 -0500

    feat: handle form submission

--- frontend/src/components/auth/Signup.tsx
+++ frontend/src/components/auth/Signup.tsx
@@ -7,14 +7,19 @@ const Signup = (): React.ReactElement => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
 
+  const onSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    console.log(`firstName: ${firstName} lastName: ${lastName}`);
+  }
+
   return (
     <Flex width="100vw" height="100vh" align="center" justify="center" background="#404040">
       <Box width="md" pt={16} pr={16} pl={16} pb={10} m={5} background="white" rounded={20}>
         <Box pb={4} textAlign="left">
-          <Heading size='lg'>Create an Account</Heading>
+          <Heading size="lg">Create an Account</Heading>
         </Box>
         <Box my={4} textAlign="left">
-          <form >
+          <form onSubmit={onSubmit}>
             <FormControl border={2} borderColor="#0B0B0B" isRequired>
               <FormLabel>First Name</FormLabel>
               <Input onChange={event => setFirstName(event.currentTarget.value)} />
@@ -24,12 +29,12 @@ const Signup = (): React.ReactElement => {
               <Input onChange={event => setLastName(event.currentTarget.value)} />
             </FormControl>
             <Center mt={4} mb={4}>
-              <Button isDisabled={firstName === "" || lastName === ""} width="full" mt={4} color="white" background="#28214C" type="submit">
+              <Button type="submit" isDisabled={firstName === "" || lastName === ""} width="full" mt={4} color="white" background="#28214C">
                 Next
               </Button>
             </Center>
             <Center>
-              <Text fontSize='sm'>Already have an account? <ChakraLink as={ReactRouterLink} to={LOGIN_PAGE} textDecoration="underline">Log In</ChakraLink></Text>
+              <Text fontSize="sm">Already have an account? <ChakraLink as={ReactRouterLink} to={LOGIN_PAGE} textDecoration="underline">Log In</ChakraLink></Text>
             </Center>
           </form>
         </Box>
