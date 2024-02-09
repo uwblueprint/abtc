3a6445acdf3112e1165675801b250a13fdd67121
Emma Huang <emmahuang.ty@gmail.com>
2024-02-08 19:59:57 -0500

    Add seeding script for seeding dev database with service requests

--- backend/typescript/prisma/seed.ts
+++ backend/typescript/prisma/seed.ts
@@ -1,44 +1,59 @@
-import { Prisma, PrismaClient, Status } from "@prisma/client";
+import { Prisma, PrismaClient, ServiceRequestType } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const userPromises = [];
+  const serviceRequestPromises: Promise<any>[] = [];
 
-  for (let i = 1; i <= 4; i++) {
-    const userNumber = i;
-    const randomUser: Prisma.userCreateInput = {
-      authId: `testVolunteer${userNumber}`,
-      email: `testVolunteer${userNumber}@gmail.com`,
-      firstName: `testVolunteerFirst${userNumber}`,
-      lastName: `testVolunteerLast${userNumber}`,
-      role: "VOLUNTEER",
-      isAccepted: Status.PENDING,
-    };
+  const users = await prisma.user.findMany();
 
-    const userCreationPromise = prisma.user.create({
-      data: randomUser,
-    });
-    userPromises.push(userCreationPromise);
-  }
-  for (let i = 1; i <= 4; i++) {
-    const userNumber = i;
-    const randomUser: Prisma.userCreateInput = {
-      authId: `testAdmin${userNumber}`,
-      email: `testAdmin${userNumber}@gmail.com`,
-      firstName: `testAdminFirst${userNumber}`,
-      lastName: `testAdminLast${userNumber}`,
-      role: "ADMIN",
-      isAccepted: Status.PENDING,
-    };
-
-    const userCreationPromise = prisma.user.create({
-      data: randomUser,
-    });
-    userPromises.push(userCreationPromise);
+  // Check if there are users to associate with service requests
+  if (users.length === 0) {
+    throw new Error("No users found for seeding service requests");
   }
 
-  await Promise.all(userPromises);
+  const serviceRequests: Prisma.serviceRequestCreateInput[] = [];
+  users.forEach((user, idx) => {
+    serviceRequests.push({
+      requestName: `Site Service Request ${idx}`,
+      requester: {
+        connect: { id: user.id },
+      },
+      location: `Location ${idx}`,
+      shiftTime: new Date(new Date().setDate(new Date().getDate() - idx)),
+      description: `Description for Service Request ${idx}`,
+      meal: `Meal ${idx}`,
+      cookingMethod: `Cooking Method ${idx}`,
+      frequency: `Frequency ${idx}`,
+      requestType: ServiceRequestType.SITE,
+    });
+  });
+
+  users.forEach((user, idx) => {
+    serviceRequests.push({
+      requestName: `Kitchen Service Request ${idx}`,
+      requester: {
+        connect: { id: user.id },
+      },
+      location: `Location ${idx}`,
+      shiftTime: new Date(new Date().setDate(new Date().getDate() - idx)),
+      description: `Description for Service Request ${idx}`,
+      meal: `Meal ${idx}`,
+      cookingMethod: `Cooking Method ${idx}`,
+      frequency: `Frequency ${idx}`,
+      requestType: ServiceRequestType.KITCHEN,
+    });
+  });
+
+  serviceRequests.forEach((request) => {
+    serviceRequestPromises.push(
+      prisma.serviceRequest.create({
+        data: request,
+      }),
+    );
+  });
+
+  await Promise.all(serviceRequestPromises);
 }
 
 main()
