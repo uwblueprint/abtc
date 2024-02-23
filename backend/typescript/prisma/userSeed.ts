12c7898c2dd645babe88dd2d12055881e9826477
Emma Huang <emmahuang.ty@gmail.com>
2024-02-22 20:31:15 -0500

    Add fire base user creation to seeding script

--- backend/typescript/prisma/userSeed.ts
+++ backend/typescript/prisma/userSeed.ts
@@ -1,15 +1,32 @@
 import { Prisma, PrismaClient, Status } from "@prisma/client";
+import * as firebaseAdmin from "firebase-admin";
 
 const prisma = new PrismaClient();
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_SVC_ACCOUNT_PRIVATE_KEY?.replace(
+      /\\n/g,
+      "\n",
+    ),
+    clientEmail: process.env.FIREBASE_SVC_ACCOUNT_CLIENT_EMAIL,
+  }),
+});
 
 async function main() {
   const userPromises = [];
 
   for (let i = 1; i <= 4; i++) {
     const userNumber = i;
-    const randomUser: Prisma.userCreateInput = {
-      authId: `testVolunteer${userNumber}`,
+    // eslint-disable-next-line no-await-in-loop
+    const firebaseUser = await firebaseAdmin.auth().createUser({
       email: `testVolunteer${userNumber}@gmail.com`,
+      password: `abtcTestPassword`,
+    });
+
+    const randomUser: Prisma.userCreateInput = {
+      authId: firebaseUser.uid,
+      email: firebaseUser.email ?? "",
       firstName: `testVolunteerFirst${userNumber}`,
       lastName: `testVolunteerLast${userNumber}`,
       role: "VOLUNTEER",
@@ -24,9 +41,15 @@ async function main() {
 
   for (let i = 1; i <= 4; i++) {
     const userNumber = i;
-    const randomUser: Prisma.userCreateInput = {
-      authId: `testAdmin${userNumber}`,
+    // eslint-disable-next-line no-await-in-loop
+    const firebaseUser = await firebaseAdmin.auth().createUser({
       email: `testAdmin${userNumber}@gmail.com`,
+      password: `abtcTestPassword`,
+    });
+
+    const randomUser: Prisma.userCreateInput = {
+      authId: firebaseUser.uid,
+      email: firebaseUser.email ?? "",
       firstName: `testAdminFirst${userNumber}`,
       lastName: `testAdminLast${userNumber}`,
       role: "ADMIN",
@@ -41,12 +64,18 @@ async function main() {
 
   for (let i = 1; i <= 2; i++) {
     const userNumber = i;
+    // eslint-disable-next-line no-await-in-loop
+    const firebaseUser = await firebaseAdmin.auth().createUser({
+      email: `testVolunteer${userNumber}Accepted@gmail.com`,
+      password: `abtcTestPassword`,
+    });
+
     const randomUser: Prisma.userCreateInput = {
-      authId: `testAdmin${userNumber}Accepted`,
-      email: `testAdmin${userNumber}Accepted@gmail.com`,
-      firstName: `testAdminFirst${userNumber}Accepted`,
-      lastName: `testAdminLast${userNumber}Accepted`,
-      role: "ADMIN",
+      authId: firebaseUser.uid,
+      email: firebaseUser.email ?? "",
+      firstName: `testVolunteerFirst${userNumber}Accepted`,
+      lastName: `testVolunteerLast${userNumber}Accepted`,
+      role: "VOLUNTEER",
       isAccepted: Status.ACCEPTED,
     };
 
@@ -58,12 +87,18 @@ async function main() {
 
   for (let i = 1; i <= 2; i++) {
     const userNumber = i;
-    const randomUser: Prisma.userCreateInput = {
-      authId: `testAdmin${userNumber}Accepted`,
+    // eslint-disable-next-line no-await-in-loop
+    const firebaseUser = await firebaseAdmin.auth().createUser({
       email: `testAdmin${userNumber}Accepted@gmail.com`,
+      password: `abtcTestPassword`,
+    });
+
+    const randomUser: Prisma.userCreateInput = {
+      authId: firebaseUser.uid,
+      email: firebaseUser.email ?? "",
       firstName: `testAdminFirst${userNumber}Accepted`,
       lastName: `testAdminLast${userNumber}Accepted`,
-      role: "VOLUNTEER",
+      role: "ADMIN",
       isAccepted: Status.ACCEPTED,
     };
 
@@ -79,5 +114,7 @@ async function main() {
 main()
   .catch((e) => console.error(e))
   .finally(async () => {
+    console.log("Seeding users complete");
     await prisma.$disconnect();
+    await firebaseAdmin.app().delete();
   });
