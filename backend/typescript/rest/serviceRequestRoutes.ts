f803381a790ba3ae424116737b037625720b0f90
Yaser <yayonoss@gmail.com>
2024-03-07 18:28:03 -0500

    POST ServiceRequest route

--- backend/typescript/rest/serviceRequestRoutes.ts
+++ backend/typescript/rest/serviceRequestRoutes.ts
@@ -3,6 +3,8 @@ import IServiceRequest from "../services/interfaces/serviceRequest";
 import ServiceRequest from "../services/implementations/serviceRequest";
 import { getErrorMessage } from "../utilities/errorUtils";
 import { isAuthorizedByRole } from "../middlewares/auth";
+import { Prisma } from '@prisma/client';
+import { getMongoUserByAuthId } from "../services/implementations/userService";
 
 const serviceRequestRouter: Router = Router();
 const serviceRequestService: IServiceRequest = new ServiceRequest();
@@ -33,6 +35,16 @@ serviceRequestRouter.get("/", async (req, res) => {
     } catch (error: unknown) {
       res.status(500).json({ error: getErrorMessage(error) });
     }
+  }
+});
+
+
+serviceRequestRouter.post("/post", async (req, res) => {
+  try {
+      const newServiceRequest = await serviceRequestService.postServiceRequest(req.body);
+      res.status(200).json(newServiceRequest);
+  } catch (error: unknown) {
+    res.status(500).json({ error: getErrorMessage(error) });
   }
 });
 
