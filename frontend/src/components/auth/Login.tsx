5f23fe85e311b70383c27ab46905e7bbf6b9f2c6
niharikasrivatsa <niharika.kasagar@gmail.com>
2024-03-20 19:59:44 -0400

    fix merge conflict errors

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { Redirect, useHistory, Link as ReactRouterLink } from "react-router-dom";
 import { Box, Heading, FormControl, Text, Input, Flex, FormLabel, Center, Link, Button } from "@chakra-ui/react";
 
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import authAPIClient from "../../APIClients/AuthAPIClient";
 import { HOME_PAGE, SIGNUP_PAGE } from "../../constants/Routes";
 import AuthContext from "../../contexts/AuthContext";
@@ -19,145 +20,137 @@ const Login = (): React.ReactElement => {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    if (error) { setError(null) }
-  }, [email, password])
+    if (error) {
+      setError(null);
+    }
+  }, [email, password]);
 
   const onLogInClick = async (event: React.FormEvent) => {
     event.preventDefault();
     try {
-      const user: AuthenticatedUser = await authAPIClient.login(email, password);
+      const user: AuthenticatedUser = await authAPIClient.login(
+        email,
+        password,
+      );
       setAuthenticatedUser(user);
     } catch (_error) {
       setError("Invalid email or password");
     }
-    const onLogInClick = async (event: React.FormEvent) => {
-      event.preventDefault();
-      try {
-        const user: AuthenticatedUser = await authAPIClient.login(
-          email,
-          password,
-        );
-        setAuthenticatedUser(user);
-      } catch (_error) {
-        setError("Invalid email or password");
-      }
-    };
-
-    const onGoogleLoginSuccess = async (idToken: string) => {
-      const user: AuthenticatedUser = await authAPIClient.loginWithGoogle(
-        idToken,
-      );
-      setAuthenticatedUser(user);
-    };
-
-    if (authenticatedUser) {
-      return <Redirect to={HOME_PAGE} />;
-    }
-
-    return (
-      <Flex
-        width="100vw"
-        height="100vh"
-        align="center"
-        justify="center"
-        background="#404040"
-      >
-        <Box
-          width="md"
-          pt={16}
-          pr={16}
-          pl={16}
-          pb={10}
-          m={5}
-          background="white"
-          rounded={20}
-        >
-          <Box pb={4} textAlign="left">
-            <Heading size="lg">Log In</Heading>
-          </Box>
-          <Box my={4} textAlign="left">
-            <form onSubmit={onLogInClick}>
-              <FormControl border={2} borderColor="#0B0B0B">
-                <FormLabel>E-mail</FormLabel>
-                <Input
-                  onChange={(event) => setEmail(event.currentTarget.value)}
-                  borderColor={error ? "red.500" : "black"}
-                />
-                {error && (
-                  <Text color="red.500" fontSize="sm" textAlign="right">
-                    {error}
-                  </Text>
-                )}
-              </FormControl>
-              <FormControl mt={6} border={2}>
-                <FormLabel>Password</FormLabel>
-                <Input
-                  type="password"
-                  onChange={(event) => setPassword(event.currentTarget.value)}
-                  borderColor={error ? "red.500" : "black"}
-                />
-                {error && (
-                  <Text color="red.500" fontSize="sm" textAlign="right">
-                    {error}
-                  </Text>
-                )}
-                <Box textAlign="right">
-                  <Link
-                    as={ReactRouterLink}
-                    to="/forgot-password"
-                    textDecoration="underline"
-                    fontSize="sm"
-                    _hover={{ color: "#28214C" }}
-                  >
-                    Forgot Password?
-                  </Link>
-                </Box>
-              </FormControl>
-              <Center mt={4} mb={4}>
-                <Button
-                  type="submit"
-                  isDisabled={email === "" || password === ""}
-                  width="full"
-                  mt={4}
-                  color="white"
-                  background="#28214C"
-                  _hover={{ opacity: "0.8" }}
-                >
-                  Log In
-                </Button>
-              </Center>
-              <Center>
-                <GoogleLogin
-                  onSuccess={(credentialResponse) => {
-                    if (credentialResponse.credential) {
-                      onGoogleLoginSuccess(credentialResponse.credential);
-                    }
-                  }}
-                  onError={() => {
-                    console.log("Login Failed");
-                  }}
-                />
-              </Center>
-              <Center>
-                <Text mt={4} fontSize="sm">
-                  Don&apos;t have an account?{" "}
-                  <Link
-                    as={ReactRouterLink}
-                    to={SIGNUP_PAGE}
-                    textDecoration="underline"
-                    fontWeight="semibold"
-                  >
-                    Register
-                  </Link>
-                </Text>
-              </Center>
-            </form>
-          </Box>
-        </Box>
-      </Flex>
-
-
-    );
   };
 
-  export default Login;
+  const onGoogleLoginSuccess = async (idToken: string) => {
+    const user: AuthenticatedUser = await authAPIClient.loginWithGoogle(
+      idToken,
+    );
+    setAuthenticatedUser(user);
+  };
+
+  if (authenticatedUser) {
+    return <Redirect to={HOME_PAGE} />;
+  }
+
+  return (
+    <Flex
+      width="100vw"
+      height="100vh"
+      align="center"
+      justify="center"
+      background="#404040"
+    >
+      <Box
+        width="md"
+        pt={16}
+        pr={16}
+        pl={16}
+        pb={10}
+        m={5}
+        background="white"
+        rounded={20}
+      >
+        <Box pb={4} textAlign="left">
+          <Heading size="lg">Log In</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
+          <form onSubmit={onLogInClick}>
+            <FormControl border={2} borderColor="#0B0B0B">
+              <FormLabel>E-mail</FormLabel>
+              <Input
+                onChange={(event) => setEmail(event.currentTarget.value)}
+                borderColor={error ? "red.500" : "black"}
+              />
+              {error && (
+                <Text color="red.500" fontSize="sm" textAlign="right">
+                  {error}
+                </Text>
+              )}
+            </FormControl>
+            <FormControl mt={6} border={2}>
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                onChange={(event) => setPassword(event.currentTarget.value)}
+                borderColor={error ? "red.500" : "black"}
+              />
+              {error && (
+                <Text color="red.500" fontSize="sm" textAlign="right">
+                  {error}
+                </Text>
+              )}
+              <Box textAlign="right">
+                <Link
+                  as={ReactRouterLink}
+                  to="/forgot-password"
+                  textDecoration="underline"
+                  fontSize="sm"
+                  _hover={{ color: "#28214C" }}
+                >
+                  Forgot Password?
+                </Link>
+              </Box>
+            </FormControl>
+            <Center mt={4} mb={4}>
+              <Button
+                type="submit"
+                isDisabled={email === "" || password === ""}
+                width="full"
+                mt={4}
+                color="white"
+                background="#28214C"
+                _hover={{ opacity: "0.8" }}
+              >
+                Log In
+              </Button>
+            </Center>
+            <Center>
+              <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                  if (credentialResponse.credential) {
+                    onGoogleLoginSuccess(credentialResponse.credential);
+                  }
+                }}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
+            </Center>
+            <Center>
+              <Text mt={4} fontSize="sm">
+                Don&apos;t have an account?{" "}
+                <Link
+                  as={ReactRouterLink}
+                  to={SIGNUP_PAGE}
+                  textDecoration="underline"
+                  fontWeight="semibold"
+                >
+                  Register
+                </Link>
+              </Text>
+            </Center>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Login;
