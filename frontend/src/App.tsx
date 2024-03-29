9c9cb1a8d23226ff45e0ce4d6d4a04ad1d1aa0c6
Yaser <yayonoss@gmail.com>
2024-03-28 20:36:23 -0400

    Created route for volunteer dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import SimpleEntityDisplayPage from "./components/pages/SimpleEntityDisplayPage"
 import NotFound from "./components/pages/NotFound";
 import UpdatePage from "./components/pages/UpdatePage";
 import SimpleEntityUpdatePage from "./components/pages/SimpleEntityUpdatePage";
+import VolunteerDashboard from "./components/pages/VolunteerDashboard";
 import * as Routes from "./constants/Routes";
 import AUTHENTICATED_USER_KEY from "./constants/AuthConstants";
 import AuthContext from "./contexts/AuthContext";
@@ -114,6 +115,11 @@ const App = (): React.ReactElement => {
                   exact
                   path={Routes.HOOKS_PAGE}
                   component={HooksDemo}
+                />
+                <PrivateRoute
+                  exact
+                  path={Routes.VOLUNTEER_DASHBOARD_PAGE}
+                  component={VolunteerDashboard}
                 />
                 <Route exact path="*" component={NotFound} />
               </Switch>
