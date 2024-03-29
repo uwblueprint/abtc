db1de1458867f9b495d63fafc4193c34128128fb
Yaser <yayonoss@gmail.com>
2024-03-28 20:24:04 -0400

    Add get request for user info in dashboard page

--- frontend/src/components/pages/VolunteerDashboard.tsx
+++ frontend/src/components/pages/VolunteerDashboard.tsx
@@ -1,10 +1,25 @@
-import React from "react";
-import { Flex, Box, Spacer, Heading } from '@chakra-ui/react';
+import React, { useEffect, useState } from "react";
+import { Flex, Box, Heading } from '@chakra-ui/react';
+import AUTHENTICATED_USER_KEY from "../../constants/AuthConstants";
 
 const VolunteerDashboard = (): React.ReactElement => {
+  const [userInfo, setUserInfo] = useState<any>(null); 
+
+  useEffect(() => {
+    const userData = localStorage.getItem(AUTHENTICATED_USER_KEY);
+
+    if (userData) {
+      const parsedUserData = JSON.parse(userData);
+      console.log(parsedUserData);
+
+      setUserInfo(userData);
+    }
+  }, []);
+
   return (
     <Flex direction="column" h="100vh">
       {/* Navbar Component Here */}
+
       <Flex bg="gray.100" pl={8} align="center" h="72px">
         <Heading size='md'>Temp Navbar</Heading>
       </Flex>
